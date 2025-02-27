63ca25b06f4b3788cafd285b50fd779844a44e38
Abhishek Singh Chuahan <cbittu349@gmail.com>
2025-02-27 23:14:27 +0530

    Update c++20 - Copy - Copy.cpp

--- c++20 - Copy - Copy.cpp
+++ c++20 - Copy - Copy.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
-void sum(){
-	int a,b,c;
-	cout<<"enter 2no";
-	cin>>a>>b;
-     c=a+b;
-     cout<<"sum="<<c;
-}
 int main(){
-	sum();
+	int a,b,c,d,e;
+	cout<<"enter any 2nd no";
+	cin>>a>>b;
+	c=a+b;
+	cout<<"sum="<<c<<endl;
+	cout<<"enter any 2nd nos ";
+	cin>>a>>b;
+	d=a*b;
+	cout<<"multi="<<d<<endl;
+	cout<<"enter any 2nd nos";
+	cin>>a>>b;
+	e=a-b;
+	cout<<"prod="<<e<<endl;
 	return 0;
 }
