4b63a3b94a7ef659c2deb1d2b494d70e4d86107f
Abhishek Singh Chuahan <cbittu349@gmail.com>
2025-02-28 13:11:59 +0530

    Update c++p13 - Copy - Copy.cpp

--- c++p13 - Copy - Copy.cpp
+++ c++p13 - Copy - Copy.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream>
+#include<iostream>
 using namespace std;
 int main (){
 	int d,w;
@@ -10,7 +10,7 @@ d=d%7;
 cout<<"enter any week"<<w<<endl<<"days is"<<d;
 return 0;
  
-}*/
+}
 /*#include<iostream>
 using namespace std;
 int main(){
