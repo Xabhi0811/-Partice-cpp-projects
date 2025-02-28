429b7db6e6d6d09f145e2f6fdbb765101bb4c634
Abhishek Singh Chuahan <cbittu349@gmail.com>
2025-02-28 13:11:11 +0530

    Update day 04 3 - Copy - Copy.cpp

--- day 04 3 - Copy - Copy.cpp
+++ day 04 3 - Copy - Copy.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream>
+#include<iostream>
 using namespace std;
 int main(){
 	int n,i,ans;
@@ -10,7 +10,7 @@ int main(){
 	 cout<<n<<" * "<<i<<" = "<<ans<<endl;}
 	 return 0;
 	
-}*/
+}
 
 /*#include<iostream>
 using namespace std;
