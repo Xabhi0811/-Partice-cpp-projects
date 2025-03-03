013133bb5ea35bcc352597bf493ae09d11b8e40a
Abhishek Singh Chuahan <cbittu349@gmail.com>
2025-03-03 23:33:56 +0530

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void add();
+int main()
+{
+	add();
+	return 0;
+}
+void add()
+{
+	int a,b,c;
+	printf("enter any no");
+	scanf("%d %d",&a,&b);
+	c=a*b;
+	printf("fuck off bitch this is your answer= %d",c);
