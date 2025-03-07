23590c244ba8001eac69d361535b606c6afd7c3f
Abhishek Singh Chuahan <cbittu349@gmail.com>
2025-03-07 23:30:54 +0530

    Update array 01 - Copy - Copy.cpp

--- array 01 - Copy - Copy.cpp
+++ array 01 - Copy - Copy.cpp
@@ -1,2 +1,15 @@
 #include<stdio.h>
-int i,j;
+int main (){
+	int i,n;
+	printf("enter your no u betichod");
+	scanf ("%d",&n);
+for (i=2;n/i;){
+
+if(n==1){
+
+printf (" it is not  prime");
+break;}
+else 
+printf(" it is prime ");}
+  return 0;
+}
