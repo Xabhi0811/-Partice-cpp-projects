c67fedaeb0c08ea07e5dbe228e32dbab79984013
Abhishek Singh Chuahan <cbittu349@gmail.com>
2025-03-07 23:29:57 +0530

    Update 2 new probkh - Copy - Copy.cpp

--- 2 new probkh - Copy - Copy.cpp
+++ 2 new probkh - Copy - Copy.cpp
@@ -4,13 +4,13 @@ int main()
 {
   char n[5][20];
   int i=0;
-  //input
+
   for(i=0;i<5;i++)
   {
     printf("Enter any string ");
     scanf("%s",n[i]);
   }
-  //display
+  
   for(i=0;i<5;i++)
   {
     printf("%s\n",n[i]);
