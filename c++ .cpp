904439e2bd1dc55db229521948938d6ae8d1540b
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-03-12 15:39:47 +0530

    Update and rename c++ .20 to c++ .cpp

--- c++ .cpp
+++ c++ .cpp
@@ -14,18 +14,3 @@ int main()
 	printf("difference of no= %d\n",c);
 } 
 
-#include<stdio.h>
-void add();
-int main()
-{
-	add();
-	return 0;
-}
-void add()
-{
-	int a,b,c;
-	printf("enter any no");
-	scanf("%d %d",&a,&b);
-	c=a*b;
-	printf("fuck off bitch this is your answer= %d",c);
-}*/
