eed75fbca31a312e2b751c92dec124c35205bfac
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-03-17 20:54:35 +0530

    Update loop 01 s e - Copy.cpp

--- loop 01 s e - Copy.cpp
+++ loop 01 s e - Copy.cpp
@@ -5,7 +5,7 @@ int main()
 	printf("Enter 2 nos ");
 	scanf("%d %d",&a,&b);
 	printf("1st value = %d 2nd value = %d\n",a,b);
-	a=a+b;//1
+	a=a+b;
 	b=a-b;//2
 	a=a-b;//3
 	printf("1st value = %d 2nd value = %d\n",a,b);
