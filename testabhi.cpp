81e1ae53ef486ae962d6bf98d2089d97f4446a97
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-15 01:14:11 +0530

    Create testabhi.cpp

---
+++ testabhi.cpp
@@ -0,0 +1,14 @@
+ 
+ #include<iostream>
+ using namespace std;
+ int main(){
+ 	cout<<"number now are you"<<endl;
+ 	int a;
+ 	cout<<"enter any number ";
+ 	cin>>a;
+ 	cout<<"number is"<<a<<endl;
+ 	clog<<"all the best for your future"<<endl;
+ 	cout<<"keep away from error"<<endl;
+ 	return 0;
+ }
+ 
