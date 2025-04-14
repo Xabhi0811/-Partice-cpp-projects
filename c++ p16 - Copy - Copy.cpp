f048c6ce6a00d7d8c34b582e4a3ee5e7cf8b7c20
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-15 01:12:39 +0530

    Update c++ p16 - Copy - Copy.cpp

--- c++ p16 - Copy - Copy.cpp
+++ c++ p16 - Copy - Copy.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream >
+#include<iostream >
 using namespace std;
 int main(){
 	int a,b,c;
@@ -10,18 +10,4 @@ int main(){
 	cout<<"1 nos is"<<a<<endl;;
 	cout<<"2 nos is "<<b;
 	return 0;
-}*/
- 
- #include<iostream>
- using namespace std;
- int main(){
- 	cout<<"number now are you"<<endl;
- 	int a;
- 	cout<<"enter any number ";
- 	cin>>a;
- 	cout<<"number is"<<a<<endl;
- 	clog<<"all the best for your future"<<endl;
- 	cout<<"keep away from error"<<endl;
- 	return 0;
- }
- 
+}
