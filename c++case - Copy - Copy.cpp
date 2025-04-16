c94efdd6498889c9198b1688b86ac3385106dc10
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-16 14:53:11 +0530

    Update c++case - Copy - Copy.cpp

--- c++case - Copy - Copy.cpp
+++ c++case - Copy - Copy.cpp
@@ -39,45 +39,7 @@ void sum::diff()
 		s.output();
 		return 0;
 }
-/*#include<iostream>
-using namespace std;
- class task{
- 	private:
- 	int a,b,c,d,e,f;
- 	public:
- 		void input();
- 		void output();
- 		void cal();
- 		void cube();
- 		void squre();
- };
-  void task:: input()
-  {
-  	cout<<"enter any num two";
-  	cin>>a>>b;
-  	
-  }
-  void task::cal(){
-   
-  	c=a*a*a;
-  	
-   	b=a*a;
-}
-   void task::output()
-   {
-   	cal();
-   	cout<<"sum of two num"<<c<<"cube of two num"<<b<<endl;
-   		
-   }
-   int main()
-   {
-   	task t();
-   	t.input();
-   	t. output();
-   	t.cal();
-   	return 0;
-   }
-*/
+
 
 
 
