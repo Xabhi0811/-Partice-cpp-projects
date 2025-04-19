f56ae7f27ad97766a76aab37ad13b2a56f3f2a79
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-19 11:32:34 +0530

    Create new2.cpp

---
+++ new2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main( ){
+	int a,i,n;
+	cout<<"enter any num";
+	cin>>n;
+	for(i=1;i<=10;i++)
+	{
+		a=n*i;
+	
+	cout<<n<<"*"<<i<<"="<<a<<endl;}
+	return 0;
+}
+
+
