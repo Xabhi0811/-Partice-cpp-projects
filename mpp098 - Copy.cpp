cbd9df4e4703fa874ba0bd720651da08dd7b0414
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-20 01:18:57 +0530

    Update mpp098 - Copy.cpp

--- mpp098 - Copy.cpp
+++ mpp098 - Copy.cpp
@@ -4,7 +4,7 @@ If derived class defines same function as defined in its
 base class, it is known as function overriding in C++. 
 It is used to achieve runtime polymorphism. 
 It enables you to provide specific implementation of the function 
-which is already provided by its base class.
+
 
 #include <iostream>  
 using namespace std;  
