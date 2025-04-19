f02908c293ae0541263fb71b4a424b521219d1c3
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-19 11:34:18 +0530

    Create new4.cpp

---
+++ new4.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+
+  int i=1,n,sum;
+cout<<"enter any num";
+cin>>n;
+for(i=1;i<=n;i++)
+{
+  
+
+}
+cout<<"sum of your natural no is "<<sum;
+return 0;}*/
+
