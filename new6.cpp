327d0d4e266099abef3802508cc7e8d741f91c0b
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-19 11:37:07 +0530

    Create new6.cpp

---
+++ new6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int i=1,n,sum;
+cout<<"enter any num";
+cin>>n;
+
+	for(i=1;i<=n;i++){
+	if(n/2==0){
+	sum=n*(n+1)/2;}
+	cout<<"sum of natural no is"<<sum;
+      }
+return 0;}
