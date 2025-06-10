eb724340bbb51e8827ee8535a1a0e40c3bcc13c1
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-06-10 10:14:47 +0530

    Create hii.cpp

---
+++ hii.cpp
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+	int main(){ 
+		 int i=1,n,f=1;
+		 {
+		 	printf("enter any num");
+		 	scanf("%d",n);
+		 	{
+		 		while(i<=n)
+		 		f=f*1;
+		 		i++;
+		 		}
+		 		printf("factorial of a num =%d\n",f);
+		 }
+		  return 0;
+		}
+	
