9da4754ad73e5a4b1dadaade6313a786a85dbf03
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-06-25 06:57:50 +0530

    Update day 9 .2 - Copy - Copy.cpp

--- day 9 .2 - Copy - Copy.cpp
+++ day 9 .2 - Copy - Copy.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
     if (n >= 1 && n <= 9) {
         string words[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         cout << words[n - 1] << endl;
