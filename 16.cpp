c89b1f3853b4f4fa4ee7dbb139f4a95d993024b9
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-11-22 18:26:17 +0530

    Add program to input and display strings

---
+++ 16.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char n[5][20];
+  int i=0;  for(i=0;i<5;i++)
+  {
+    printf("Enter any string ");
+    scanf("%s",n[i]);
+  }
+ 
+  for(i=0;i<5;i++)
+  {
+    printf("%s\n",n[i]);
+  }
+  getch();
+  return(0);
+}
