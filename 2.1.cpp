5ca322470bc8066b4ade73a5c3fd7cb5d6cf5c51
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-11-25 19:53:46 +0530

    Update 2.1.cpp

---
+++ 2.1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>  
+using namespace std;  
+class hello 
+{  
+    public:  
+	void abc()
+	{    
+		cout<<"abc function of base class"<<endl;    
+    }      
+};   
+class hi: public hello    
+{    
+ 	public:  
+  	void abc()    
+  	{    
+       cout<<"abc function of derived class"<<endl;    
+    }    
+};  
+int main() 
+{  
+   hi h;    
+   h.abc();  
+   return 0;  
+}
