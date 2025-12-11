145510cfef8fbc4b71be609139a2b62cc4beab7b
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-12-11 22:43:33 +0530

    Implement Average class to calculate average of three numbers

---
+++ bahar.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Average {
+public:
+    static void calculateAverage() {
+        double num1, num2, num3;
+        cout << "Enter first number: ";
+        cin >> num1;
+        cout << "Enter second number: ";
+        cin >> num2;
+        cout << "Enter third number: ";
+        cin >> num3;
+        
+        double average = (num1 + num2 + num3) / 3;
+        
+        cout << "The average of the three numbers is: " << average << endl;
+    }
+};
+
+int main() {
+    // Call the static member function directly without creating an object
+    Average::calculateAverage();
+    return 0;
+}
