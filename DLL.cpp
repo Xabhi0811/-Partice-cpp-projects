adaee10b962585d1a29f4d862c41989630f5f844
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-12-24 17:46:41 +0530

    Update DLL.cpp

--- DLL.cpp
+++ DLL.cpp
@@ -1,20 +1,34 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-  char n[5][20];
-  int i=0;
-  //input
-  for(i=0;i<5;i++)
-  {
-    printf("Enter any string ");
-    scanf("%s",n[i]);
-  }
-  //display
-  for(i=0;i<5;i++)
-  {
-    printf("%s\n",n[i]);
-  }
-  getch();
-  return(0);
+#include <bits/stdc++.h>
+using namespace std;
+
+void graphSpecialSum(vector<int>& arr) {
+    long long current = 0, best = LLONG_MIN;
+
+    cout << "Index | Value | Transformed | CurrentSum | Graph\n";
+    cout << "------------------------------------------------------\n";
+
+    for (int i = 0; i < arr.size(); i++) {
+        long long val = (i % 2 == 0) ? arr[i] : -arr[i];
+
+        current = max(val, current + val);
+        best = max(best, current);
+
+        cout << setw(5) << i << " | "
+             << setw(5) << arr[i] << " | "
+             << setw(11) << val << " | "
+             << setw(10) << current << " | ";
+
+       
+        int bars = abs(current);
+        for (int j = 0; j < bars; j++) cout << "#";
+        cout << "\n";
+    }
+
+    cout << "\nMaximum Special Sum = " << best << endl;
+}
+
+int main() {
+    vector<int> arr = {6, -3, 3, 4};
+    graphSpecialSum(arr);
+    return 0;
 }
