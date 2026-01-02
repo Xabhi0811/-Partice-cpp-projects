e3438da8b03e7874dfaf6a3e864072b65e1f7563
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-02 20:43:15 +0530

    Implement unbounded knapsack algorithm in main.cpp

--- day 01 part - Copy - Copy.cpp
+++ day 01 part - Copy - Copy.cpp
@@ -1,10 +1,22 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-      int i,n;
-      cout<<"enter any num";
-      cin>>n;
-      for(i=1;i<=n;i--)
-      cout<<i;
-      
-	  return 0;}
+
+int unboundedKnapsack(int W, vector<int>& wt, vector<int>& val) {
+    vector<int> dp(W+1, 0);
+
+    for(int w = 0; w <= W; w++) {
+        for(int i = 0; i < wt.size(); i++) {
+            if(wt[i] <= w)
+                dp[w] = max(dp[w], val[i] + dp[w - wt[i]]);
+        }
+    }
+    return dp[W];
+}
+
+int main() {
+    vector<int> wt = {2, 4, 6};
+    vector<int> val = {5, 11, 13};
+    int W = 10;
+
+    cout << unboundedKnapsack(W, wt, val);
+}
