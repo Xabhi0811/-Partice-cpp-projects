99f874eb57aaa37b97db928e8e1baf94c2065203
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-05 00:20:38 +0530

    Update ptq4 - Copy.cpp

--- ptq4 - Copy.cpp
+++ ptq4 - Copy.cpp
@@ -1,16 +1,37 @@
-//Q17
-#include<stdio.h>
-int main()
-{
-	int a;
-	printf("Enter any no ");
-	scanf("%d",&a);
-	if(a==0)
-	printf("no is zero");
-	else 
-	  if(a>0)
-	  printf("no is +ve");
-	  else
-	  printf("no is -ve");
-	return 0;
+#include <bits/stdc++.h>
+using namespace std;
+
+string printLCS(string a, string b) {
+    int n = a.size(), m = b.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(a[i-1]==b[j-1])
+                dp[i][j]=1+dp[i-1][j-1];
+            else
+                dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    string lcs="";
+    int i=n, j=m;
+    while(i>0 && j>0){
+        if(a[i-1]==b[j-1]){
+            lcs.push_back(a[i-1]);
+            i--; j--;
+        }
+        else if(dp[i-1][j] > dp[i][j-1])
+            i--;
+        else
+            j--;
+    }
+
+    reverse(lcs.begin(), lcs.end());
+    return lcs;
+}
+
+int main(){
+    string s1="ABCBDAB", s2="BDCAB";
+    cout << "LCS = " << printLCS(s1,s2) << endl;
 }
