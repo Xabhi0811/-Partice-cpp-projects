15fff562c2fc88c83af1e548369024589ec13c3c
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-21 20:40:57 +0530

    Update ptq15 - Copy.cpp

--- ptq15 - Copy.cpp
+++ ptq15 - Copy.cpp
@@ -1,13 +1,34 @@
-#include<stdio.h>
-int main()
-{
-	char ch;
-	printf("Enter any character ");
-	scanf("%c",&ch);
-	if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
-	//if((ch>=65 && ch<=90) || (ch>=97 && ch<=122))
-	printf("it is an alphabet");
-	else
-	printf("it is not an alphabet");
-	return 0;
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item {
+    int value, weight;
+};
+
+bool compare(Item a, Item b) {
+    return (double)a.value/a.weight > (double)b.value/b.weight;
+}
+
+double fractionalKnapsack(int W, vector<Item>& items) {
+    sort(items.begin(), items.end(), compare);
+
+    double totalValue = 0.0;
+
+    for(auto &item : items) {
+        if(W >= item.weight) {
+            W -= item.weight;
+            totalValue += item.value;
+        } else {
+            totalValue += item.value * ((double)W / item.weight);
+            break;
+        }
+    }
+    return totalValue;
+}
+
+int main() {
+    vector<Item> items = {{60,10}, {100,20}, {120,30}};
+    int W = 50;
+
+    cout << fractionalKnapsack(W, items);
 }
