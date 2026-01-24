f04e19457060a84d52dbd3fac3e85b4392bc405d
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-24 19:50:44 +0530

    Update day 03 c++ - Copy - Copy.cpp

--- day 03 c++ - Copy - Copy.cpp
+++ day 03 c++ - Copy - Copy.cpp
@@ -1,10 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
- int i,n;
- cout<<"enter any num";
- cin>>n;
- for(i=n;i<=10;i*2)
- cout<<i+2<<endl;
- return 0;
+
+int main() {
+    int x, y, z;
+    cout << "Enter dimensions (X Y Z): ";
+    cin >> x >> y >> z;
+
+    int matrix[10][10][10];  
+    int evenCount = 0, oddCount = 0;
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                cin >> matrix[i][j][k];
+                if (matrix[i][j][k] % 2 == 0)
+                    evenCount++;
+                else
+                    oddCount++;
+            }
+        }
+    }
+
+    cout << "\nElements of 3D Matrix:\n";
+    for (int i = 0; i < x; i++) {
+        cout << "Layer " << i << ":\n";
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                cout << matrix[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    cout << "\nTotal Even Elements = " << evenCount << endl;
+    cout << "Total Odd Elements  = " << oddCount << endl;
+
+    return 0;
 }
