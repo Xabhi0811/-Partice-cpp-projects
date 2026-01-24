bedabf8a119f2e2eaa560542ac062c65e5cb6b70
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-24 19:50:28 +0530

    Add 3D matrix input and even/odd count functionality

--- day 02 part - Copy - Copy.cpp
+++ day 02 part - Copy - Copy.cpp
@@ -1,12 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-	int a,b;
-	cout<<"enter any num";
-	cin>>a>>b;
-	a=a+b;
-	b=a-b;
-a=a-b;
-	cout<<"enter any num"<<a<<"b="<<b;
-	return 0;}
+
+int main() {
+    int x, y, z;
+    cout << "Enter dimensions (X Y Z): ";
+    cin >> x >> y >> z;
+
+    int matrix[10][10][10];  
+    int evenCount = 0, oddCount = 0;
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                cin >> matrix[i][j][k];
+                if (matrix[i][j][k] % 2 == 0)
+                    evenCount++;
+                else
+                    oddCount++;
+            }
+        }
+    }
+
+    cout << "\nElements of 3D Matrix:\n";
+    for (int i = 0; i < x; i++) {
+        cout << "Layer " << i << ":\n";
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                cout << matrix[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    cout << "\nTotal Even Elements = " << evenCount << endl;
+    cout << "Total Odd Elements  = " << oddCount << endl;
+
+    return 0;
+}
