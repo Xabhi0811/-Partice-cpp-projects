0170d84a64abb2fafe20dbfd002b6601ed0a6437
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-24 19:51:00 +0530

    Add 3D matrix input and even/odd counting

--- day 04 - Copy - Copy.cpp
+++ day 04 - Copy - Copy.cpp
@@ -1,64 +1,40 @@
-/*#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int m,i,n,sum;
-	cout<<"enter any num";
-	cin>>n;
-	cout<<"enter any end value";
-	cin>>m;
-	for(i=n;i>m;i++)
-	{ 
-	     cout<<i;
-		if(n>m);
-		{ sum=m+n;
-		}
-		cout<<"the sum of all nature no"<<sum;
-	}
-	return 0;
-	
-}*/
 
-#include<iostream>
-using namespace std;
-  int main()
-{
-	int i,m;
-	cout<<"enter any num";
-	cin>>m;
-	for(i=m;i<=(m*10);i+m)
-	{ cout<<i;
-	
-	
-	}return 0;
-	
+int main() {
+    int x, y, z;
+    cout << "Enter dimensions (X Y Z): ";
+    cin >> x >> y >> z;
+
+    int matrix[10][10][10];  
+    int evenCount = 0, oddCount = 0;
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                cin >> matrix[i][j][k];
+                if (matrix[i][j][k] % 2 == 0)
+                    evenCount++;
+                else
+                    oddCount++;
+            }
+        }
+    }
+
+    cout << "\nElements of 3D Matrix:\n";
+    for (int i = 0; i < x; i++) {
+        cout << "Layer " << i << ":\n";
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                cout << matrix[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    cout << "\nTotal Even Elements = " << evenCount << endl;
+    cout << "Total Odd Elements  = " << oddCount << endl;
+
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
