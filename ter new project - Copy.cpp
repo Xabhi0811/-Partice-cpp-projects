7d2d427f3e5d52a704b4bbaf4b8e081af5d36e17
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-26 13:33:03 +0530

    Implement fractional knapsack algorithm in C++

--- ter new project - Copy.cpp
+++ ter new project - Copy.cpp
@@ -1,17 +1,34 @@
-#include <stdio.h>
-int main(){ 
-int i,j;
-char A,B,C,D,E;
-	for(i=1;i<=5;i++)
-	{ for (j=1;j<=5;j++)
-		{
-			
-			printf("%d ",j);
-		}
-		
-		printf("\n");
+#include <bits/stdc++.h>
+using namespace std;
 
-} 
-	return 0;
+struct Item {
+    int value, weight;
+};
+
+bool compare(Item a, Item b) {
+    return (double)a.value/a.weight > (double)b.value/b.weight;
 }
 
+double fractionalKnapsack(int W, vector<Item>& items) {
+    sort(items.begin(), items.end(), compare);
+
+    double totalValue = 0.0;
+
+    for(auto &item : items) {
+        if(W >= item.weight) {
+            W -= item.weight;
+            totalValue += item.value;
+        } else {
+            totalValue += item.value * ((double)W / item.weight);
+            break;
+        }
+    }
+    return totalValue;
+}
+
+int main() {
+    vector<Item> items = {{60,10}, {100,20}, {120,30}};
+    int W = 50;
+
+    cout << fractionalKnapsack(W, items);
+}
