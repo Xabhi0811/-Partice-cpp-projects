4cb4ca9b3ed358a107d6c56e5ba09a504c28dcde
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-26 13:33:44 +0530

    Update test nested - Copy.cpp

--- test nested - Copy.cpp
+++ test nested - Copy.cpp
@@ -1,17 +1,34 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
 
-int i=1,j;
-for(i=1;i<=5;i++){
-	
-	for(j=1;j<=5;j++)
-	{
-		cout<<i;
-	}
-	cout<<j<<endl;
+struct Item {
+    int value, weight;
+};
+
+bool compare(Item a, Item b) {
+    return (double)a.value/a.weight > (double)b.value/b.weight;
 }
 
-return 0;
+double fractionalKnapsack(int W, vector<Item>& items) {
+    sort(items.begin(), items.end(), compare);
+
+    double totalValue = 0.0;
+
+    for(auto &item : items) {
+        if(W >= item.weight) {
+            W -= item.weight;
+            totalValue += item.value;
+        } else {
+            totalValue += item.value * ((double)W / item.weight);
+            break;
+        }
+    }
+    return totalValue;
 }
-      
+
+int main() {
+    vector<Item> items = {{60,10}, {100,20}, {120,30}};
+    int W = 50;
+
+    cout << fractionalKnapsack(W, items);
+}
