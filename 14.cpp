10cc17ef898979f5c9857e48342f2903129ca906
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-27 17:02:21 +0530

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -1,20 +1,34 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-  char n[5][20];
-  int i=0;
-  //input
-  for(i=0;i<5;i++)
-  {
-    printf("Enter any string ");
-    scanf("%s",n[i]);
-  }
-  //display
-  for(i=0;i<5;i++)
-  {
-    printf("%s\n",n[i]);
-  }
-  getch();
-  return(0);
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item {
+    int value, weight;
+};
+
+bool compare(Item a, Item b) {
+    return (double)a.value/a.weight > (double)b.value/b.weight;
+}
+
+double fractionalKnapsack(int W, vector<Item>& items) {
+    sort(items.begin(), items.end(), compare);
+
+    double totalValue = 0.0;
+
+    for(auto &item : items) {
+        if(W >= item.weight) {
+            W -= item.weight;
+            totalValue += item.value;
+        } else {
+            totalValue += item.value * ((double)W / item.weight);
+            break;
+        }
+    }
+    return totalValue;
+}
+
+int main() {
+    vector<Item> items = {{60,10}, {100,20}, {120,30}};
+    int W = 50;
+
+    cout << fractionalKnapsack(W, items);
 }
