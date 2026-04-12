c9d3b283a357f3ec44d7a4b2a1ff1d7b09216aab
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-04-12 19:28:16 +0530

    double linklist

--- link.cpp
+++ link.cpp
@@ -1,22 +1,30 @@
 #include<iostream>
 using namespace std;
+class node{
+	public:
+	int data;
+	node* prev;
+	node* next;
+	
+	node(int val){
+		data = val ;
+		prev= next = nullptr;
+	}
+};
 
-class node {
-public:
-    int data;
-    node* next;
+class douuble{
 
-    node(int data){
-        this->data = data;
-        this->next = nullptr;  
-    }
-}; 
+node * head ;
+ node * tail;
+	 public:	 
+	 bouble(){
+	 	 head = tail = nullptr;
+	 }
+};
 
 int main(){
-    node* node1 = new node(10);
-
-    cout << node1->data << endl;
-    cout << node1->next << endl;
-
-    return 0;
+	node* node1 = new node(10);
+	cout<<node1->data<<endl;
+	cout<<node1->next<<endl;
+	return 0;
 }
