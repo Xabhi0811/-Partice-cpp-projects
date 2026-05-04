b36aab6ce3d75ae23b12db8eee2fe0a77eb1f887
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-05-04 21:08:30 +0530

    sorted arrary

---
+++ sorted arr.cpp
@@ -0,0 +1,11 @@
+class Solution{	
+	public:
+		bool isSorted(vector<int>& nums){
+			 int n = nums.size();
+               if(nums[i] > nums[i + 1]){
+                return false;
+               
+             }
+             return true;
+		}
+};
