6be217b86f64b1b00ea923847472195f6429370b
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-05-05 02:40:37 +0530

    missing number slove raat ko 3 baje

---
+++ missing number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0, n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+        }
+
+        int expected = n * (n + 1) / 2;
+
+        return expected - sum;   
+    }
+};
