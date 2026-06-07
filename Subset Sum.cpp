9a86dd630cbdbee25b4ac90b550b11eb45e0e33a
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-06-07 11:04:17 +0530

    done 2

--- Subset Sum.cpp
+++ Subset Sum.cpp
@@ -5,11 +5,15 @@ bool subsetSum(int arr[], int n, int sum) {
     bool dp[n+1][sum+1];
 
 
-    
+
     for (int i=0; i<=n; i++) dp[i][0] = true;
     for (int j=1; j<=sum; j++) dp[0][j] = false;
 
     for (int i=1; i<=n; i++) {
+
+        
+
+
 
         
         for (int j=1; j<=sum; j++) {
